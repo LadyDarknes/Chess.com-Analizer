52ac8030ba8f5ca7bc3851b85eec7dd9aff30b8b
Lady of Darkness <sptovaknjo@gmail.com>
2026-02-08 18:06:00 +0300

    first commit

---
+++ inject.bat
@@ -0,0 +1,15 @@
+@echo off
+title MAIA
+color 0a
+
+echo [1/3] Clonning
+taskkill /f /im chrome.exe >nul 2>&1
+
+echo [2/3] Injector baslatiliyor...
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\sel_temp" "https://www.chess.com/play/online"
+
+echo [3/3] Bot baslatiliyor...
+echo.
+python main.py
+
+pause
